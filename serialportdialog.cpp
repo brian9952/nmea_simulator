42d7e5b8de66b63d022f7081f90f2c7026652b0d
brian9952 <fbriann@gmail.com>
2022-02-28 19:29:25 +0700

    port settings revision

--- serialportdialog.cpp
+++ serialportdialog.cpp
@@ -5,6 +5,9 @@
 SerialPortDialog::SerialPortDialog(QWidget *parent) : QDialog(parent)
 {
 
+    PortConfigs *configs = new PortConfigs;
+    configs->baudRate = QSerialPort::Baud115200;
     QSerialPort *port = new QSerialPort;
+    port->setBaudRate(configs->baudRate);
 
 }
--- serialportdialog.h
+++ serialportdialog.h
@@ -16,59 +16,13 @@ public:
 
 private:
 
-    enum BaudRate {
-        Baud1200 = 1200,
-        Baud2400 = 2400,
-        Baud4800 = 4800,
-        Baud9600 = 9600,
-        Baud19200 = 19200,
-        Baud38400 = 38400,
-        Baud57600 = 57600,
-        Baud76800 = 76800,
-        Baud115200 = 115200,
-        Baud230400 = 230400,
-        Baud460800 = 460800,
-        Baud576000 = 576000
-    };
-
-    enum DataBits {
-        Data5,
-        Data6,
-        Data7,
-        Data8,
-        UnknownDataBits
-    };
-
-    enum Parity {
-        NoParity,
-        EvenParity,
-        OddParity,
-        SpaceParity,
-        MarkParity,
-        UnknownParity
-    };
-
-    enum StopBits {
-        OneStop,
-        OneAndHalfStop,
-        TwoStop,
-        UnknownStopBits
-    };
-
-    enum FlowControl {
-        NoFlowControl,
-        HardwareControl,
-        SoftwareControl,
-        UnknownFlowControl
-    };
-
     struct PortConfigs {
         QString portName;
-        int baudRate;
-        DataBits dataBits;
-        Parity parity;
-        StopBits stopBits;
-        FlowControl flowControl;
+        QSerialPort::BaudRate baudRate;
+        QSerialPort::DataBits dataBits;
+        QSerialPort::Parity parity;
+        QSerialPort::StopBits stopBits;
+        QSerialPort::FlowControl flowControl;
     };
 
 };
