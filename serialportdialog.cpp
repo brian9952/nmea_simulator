312189fe8f6c74d0b685e973f6d4a3d1153d17d9
brian9952 <63029412+brian9952@users.noreply.github.com>
2022-03-02 20:04:17 +0700

    Update serialportdialog.cpp

--- serialportdialog.cpp
+++ serialportdialog.cpp
@@ -5,9 +5,137 @@
 SerialPortDialog::SerialPortDialog(QWidget *parent) : QDialog(parent)
 {
 
-    PortConfigs *configs = new PortConfigs;
+    configs = new PortConfigs;
+
+    // create dialog layout
+    createLayout();
+    
     configs->baudRate = QSerialPort::Baud115200;
     QSerialPort *port = new QSerialPort;
     port->setBaudRate(configs->baudRate);
 
+}
+
+void SerialPortDialog::detectSerialPorts(){
+
+    QSerialPortInfo *serialInfo = new QSerialPortInfo;
+    QList<QSerialPortInfo> sports;
+    
+    sports = serialInfo->availablePorts();
+    
+    for(int i = 0; i < sports.length(); i++){
+        serialPorts.append(sports[i].portName());
+    }
+    
+    delete serialInfo;
+    
+}
+
+void SerialPortDialog::createLayout(){
+
+    detectSerialPorts();
+    
+    // create port name label and combobox
+    portNameLabel = new QLabel(tr("Port Name: "));
+    portNameCombobox = new QComboBox();
+    for(int i = 0; i < serialPorts.length(); i++){
+        portNameCombobox->addItem(serialPorts[i]);
+    }
+    
+    // create layout
+    QHBoxLayout *portNameLayout = new QHBoxLayout;
+    portNameLayout->addWidget(portNameLabel);
+    portNameLayout->addWidget(portNameCombobox);
+    
+    // create baud rate configs
+    baudRateLabel = new QLabel(tr("Baud Rate: "));
+    baudRateCombobox = new QComboBox();
+    baudRateCombobox->addItem("1200");
+    baudRateCombobox->addItem("2400");
+    baudRateCombobox->addItem("4800");
+    baudRateCombobox->addItem("9600");
+    baudRateCombobox->addItem("19200");
+    baudRateCombobox->addItem("38400");
+    baudRateCombobox->addItem("57600");
+    baudRateCombobox->addItem("115200");
+    
+    // create layout
+    QHBoxLayout *baudRateLayout = new QHBoxLayout;
+    baudRateLayout->addWidget(baudRateLabel);
+    baudRateLayout->addWidget(baudRateCombobox);
+    
+    // create data bits configs
+    dataBitsLabel = new QLabel(tr("Data Bits: "));
+    dataBitsCombobox = new QComboBox();
+    dataBitsCombobox->addItem("5");
+    dataBitsCombobox->addItem("6");
+    dataBitsCombobox->addItem("7");
+    dataBitsCombobox->addItem("8");
+    
+    // create layout
+    QHBoxLayout *dataBitsLayout = new QHBoxLayout;
+    dataBitsLayout->addWidget(dataBitsLabel);
+    dataBitsLayout->addWidget(dataBitsCombobox);
+    
+    // create parity configs
+    parityLabel = new QLabel(tr("Parity: "));
+    parityCombobox = new QComboBox();
+    parityCombobox->addItem("No Parity");
+    parityCombobox->addItem("Even Parity");
+    parityCombobox->addItem("Odd Parity");
+    parityCombobox->addItem("Mark Parity");
+    
+    // create layout
+    QHBoxLayout *parityLayout = new QHBoxLayout;
+    parityLayout->addWidget(parityLabel);
+    parityLayout->addWidget(parityCombobox);
+    
+    // create stop bits configs
+    stopBitsLabel = new QLabel(tr("Stop Bits: "));
+    stopBitsCombobox = new QCombobox();
+    stopBitsCombobox->addItem("One Stop");
+    stopBitsCombobox->addItem("One and Half Stop");
+    stopBitsCombobox->addItem("Two Stop");
+    
+    // create layout
+    QHBoxLayout *stopBitsLayout = new QHBoxLayout;
+    stopBitsLayout->addWidget(stopBitsLabel);
+    stopBitsLayout->addWidget(stopBitsCombobox);
+    
+    // create flow control configs
+    flowControlabel = new QLabel(tr("Flow Control"));
+    flowControlCombobox = new QCombobox();
+    flowControlCombobox->addItem("No Flow Control");
+    flowControlCombobox->addItem("Hardware Control");
+    flowControlCombobox->addItem("Software Control");
+    
+    // create left three labels and dropdown box
+    QVBoxLayout *leftLayout = new QVBoxLayout;
+    leftLayout->addLayout(portNameLayout);
+    leftLayout->addLayout(baudRateLayout);
+    leftLayout->addLayout(dataBitsLayout);
+    
+    QVBoxLayout *rightLayout = new QVBoxLayout;
+    rightLayout->addLayout(parityLayout);
+    rightLayout->addLayout(stopBitsLayout);
+    rightLayout->addLayout(flowControlLayout);
+    
+    QHBoxLayout *configsLayout = new QHBoxLayout;
+    configsLayout->addLayout(leftLayout);
+    configsLayout->addLayout(rightLayout);
+    
+    // create buttons
+    applyButton = new QPushButton(tr("Apply"));
+    cancelButton = new QPushButton(tr("Cancel"));
+    
+    // create layout
+    QHBoxLayout *buttonLayout = new QHBoxLayout;
+    buttonLayout->addWidget(applyButton);
+    buttonLayout->addWidget(cancelButton);
+    
+    // create main layout
+    QVBoxLayout *mainLayout = new QVBoxLayout;
+    mainLayout->addLayout(configsLayout);
+    mainLayout->addLayout(buttonLayout);
+    
 }
